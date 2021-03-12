fd20e84632b151ef52c3cd16f2f0687f4be67d9e
Aldy 'ADK' Andika <aldy.andika@gmail.com>
2021-03-12 08:59:37 +0700

    add tooltip

--- src/components/ShareContainer.tsx
+++ src/components/ShareContainer.tsx
@@ -1,3 +1,4 @@
+import ReactTooltip from 'react-tooltip';
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -16,32 +17,53 @@ interface Props {
 
 const ShareContainer = ({ url }: Props) => {
   return (
-    <div style={{display: 'flex', justifyContent: 'center', marginTop: '40vh'}}>
-      <FacebookShareButton
-        url={'aldyandika.id'}
-        resetButtonStyle={true}
-        quote='Shared from https://seeho.me'
-        hashtag=',#seehome'
-      >
-        <FacebookIcon size={50} iconFillColor='#1877f2' bgStyle={{ fill: 'transparent' }} />
-      </FacebookShareButton>
-      <TwitterShareButton
-        url={url}
-        resetButtonStyle={true}
-        title='Seehome Shortlink'
-        via='Seehome'
-        hashtags={['seehome','shortlinks']}
-      >
-        <TwitterIcon size={50} iconFillColor='#1877f2' bgStyle={{ fill: 'transparent' }} />
-      </TwitterShareButton>
-      <EmailShareButton
-        url={url}
-        resetButtonStyle={true}
-        subject="TestSeeho.me"
-        body={`\n\n\n\nShared from <a href="https://seeho.me">seehome</a>\n\n`}
-      >
-        <EmailIcon size={50} iconFillColor='#1877f2' bgStyle={{ fill: 'transparent' }} />
-      </EmailShareButton>
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '40vh' }}>
+      <div data-tip data-for="fbShareButton">
+        <FacebookShareButton
+          url={'aldyandika.id'}
+          resetButtonStyle={true}
+          quote='Shared from https://seeho.me'
+          hashtag=',#seehome'
+        >
+          <FacebookIcon size={50} iconFillColor='#1877f2' bgStyle={{ fill: 'transparent' }} />
+        </FacebookShareButton>
+        <ReactTooltip id='fbShareButton' backgroundColor='transparent'>
+          <div style={{ border: '2px solid black', backgroundColor: 'white', padding: 10, borderRadius: 5, color: 'black' }}>
+              Share to Facebook
+          </div>
+        </ReactTooltip>
+      </div>
+      <div data-tip data-for="twitterShareButton">
+        <TwitterShareButton
+          url={url}
+          resetButtonStyle={true}
+          title='Seehome Shortlink'
+          via='Seehome'
+          hashtags={['seehome', 'shortlinks']}
+        >
+          <TwitterIcon size={50} iconFillColor='#1877f2' bgStyle={{ fill: 'transparent' }} />
+        </TwitterShareButton>
+        <ReactTooltip id='twitterShareButton' backgroundColor='transparent'>
+          <div style={{ border: '2px solid black', backgroundColor: 'white', padding: 10, borderRadius: 5, color: 'black' }}>
+              Share to Twitter
+          </div>
+        </ReactTooltip>
+      </div>
+      <div data-tip data-for="emailShareButton">
+        <EmailShareButton
+          url={url}
+          resetButtonStyle={true}
+          subject="TestSeeho.me"
+          body={`\n\n\n\nShared from <a href="https://seeho.me">seehome</a>\n\n`}
+        >
+          <EmailIcon size={50} iconFillColor='#1877f2' bgStyle={{ fill: 'transparent' }} />
+        </EmailShareButton>
+        <ReactTooltip id='emailShareButton' backgroundColor='transparent'>
+          <div style={{ border: '2px solid black', backgroundColor: 'white', padding: 10, borderRadius: 5, color: 'black' }}>
+              Share to Email
+          </div>
+        </ReactTooltip>
+      </div>
     </div >
   )
 }
